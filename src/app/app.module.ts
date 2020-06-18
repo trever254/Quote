92ef2d4d38f669b2fd2e6736c1035a564fd1ec51
trever <toakumu@gmail.com>
2020-06-18 18:52:23 +0300

    added changes on app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms"
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
+import { QuoteComponent } from './quote/quote.component';
+import { HighlightDirective } from './highlight.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    DetailsComponent,
+    QuoteComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    QuoteComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
