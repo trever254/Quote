ec0aee679a1345492f398610499c6849362950b2
trever <toakumu@gmail.com>
2020-06-18 18:57:35 +0300

    added component quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormComponent } from './form/form.component';
 import { DetailsComponent } from './details/details.component';
 import { QuoteComponent } from './quote/quote.component';
 import { HighlightDirective } from './highlight.directive';
+import { QuotesComponent } from './quotes/quotes.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HighlightDirective } from './highlight.directive';
     FormComponent,
     DetailsComponent,
     QuoteComponent,
-    HighlightDirective
+    HighlightDirective,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
