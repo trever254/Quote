e6d718d620d8713fca9bc3cc6e184873482bf819
trever <toakumu@gmail.com>
2020-06-19 07:03:52 +0300

    added changes on detail component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from "../quote"
+import { QuotesComponent } from '../quotes/quotes.component';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-
+  @Input() quote:Quote
   constructor() { }
 
   ngOnInit(): void {
